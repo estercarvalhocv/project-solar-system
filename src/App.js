001fd6fccc3098ef94b8ee85b37486293a49151c
Ester Borges de Carvalho <esterborgesdecarvalho@gmail.com>
2022-05-14 16:39:31 -0300

    require 2 OK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
-
 export default App;
